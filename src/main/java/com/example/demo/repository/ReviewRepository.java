3255657e96b2c21a655c7f38715794afc37c440a
woojin <sun65568@hanmail.net>
2023-06-23 15:56:26 +0900

    리뷰 작성 기능 추가(06/23)

--- src/main/java/com/example/demo/repository/ReviewRepository.java
+++ src/main/java/com/example/demo/repository/ReviewRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.repository;
 
-public class ReviewRepository {
+import com.example.demo.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
 }
--- src/main/java/com/example/demo/service/ReviewService.java
+++ src/main/java/com/example/demo/service/ReviewService.java
@@ -1,4 +1,32 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.ReviewDTO;
+import com.example.demo.entity.Review;
+import com.example.demo.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Transactional
+@Slf4j
+@RequiredArgsConstructor
+
 public class ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    // 리뷰 작성(POST)
+    public boolean insertReview(String reviewTitle, String reviewContent, String reviewImg) {
+        Review review = new Review();
+        review.setReviewTitle(reviewTitle);
+        review.setReviewContent(reviewContent);
+        review.setReviewImg(reviewImg);
+        review.setReviewWrittenTime(LocalDateTime.now());
+        reviewRepository.save(review);
+        return true;
+    }
 }
