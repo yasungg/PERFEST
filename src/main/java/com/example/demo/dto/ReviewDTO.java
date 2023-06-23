3e3019b725162c8020e97b08c28eafc89c4bf343
woojin <sun65568@hanmail.net>
2023-06-23 15:38:30 +0900

    리뷰 파일 추가(06/23)

---
+++ src/main/java/com/example/demo/dto/ReviewDTO.java
@@ -0,0 +1,10 @@
+package com.example.demo.dto;
+
+import java.time.LocalDateTime;
+
+public class ReviewDTO {
+    private String reviewTitle;
+    private LocalDateTime reviewWrittenTime;
+    private String reviewImg;
+    private String reviewContent;
+}
