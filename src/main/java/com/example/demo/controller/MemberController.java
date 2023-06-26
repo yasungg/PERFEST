9219cad1460cee2622704bb8a9fb00fbe9e0ec7f
yasungg <dragnx1026@gmail.com>
2023-06-26 18:34:26 +0900

    곽용석 // 6/26 commit, 현재 카카오로그인 진행 중.

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,12 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.MemberDTO;
 import com.example.demo.dto.memberDTOs.MemberRequestDTO;
 import com.example.demo.dto.memberDTOs.MemberResponseDTO;
 import com.example.demo.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000", allowedHeaders= "*")
@@ -19,18 +23,17 @@ public class MemberController {
     ResponseEntity<MemberResponseDTO> signup(@RequestBody MemberRequestDTO memberRequestDTO) {
         return ResponseEntity.ok(memberService.signup(memberRequestDTO));
     }
-    private final MemberService memberService;
     // 이메일로 회원 조회
-    @GetMapping("/email")
-    public ResponseEntity<List<MemberDTO>> getMemberInfoByEmail(@RequestParam String email) {
-        List<MemberDTO> memberList = memberService.getMemberByEmail(email);
-        return new ResponseEntity<>(memberList, HttpStatus.OK);
-    }
-
-    // 회원 삭제
-    @PostMapping("/delete")
-    public ResponseEntity<String> deleteMember(@RequestParam Long memberId) {
-        memberService.deleteMember(memberId);
-        return new ResponseEntity<>("회원탈퇴여부 Y 변경", HttpStatus.OK);
-    }
+//    @GetMapping("/email")
+//    public ResponseEntity<List<MemberDTO>> getMemberInfoByEmail(@RequestParam String email) {
+//        List<MemberDTO> memberList = memberService.getMemberByEmail(email);
+//        return new ResponseEntity<>(memberList, HttpStatus.OK);
+//    }
+//
+//    // 회원 삭제
+//    @PostMapping("/delete")
+//    public ResponseEntity<String> deleteMember(@RequestParam Long memberId) {
+//        memberService.deleteMember(memberId);
+//        return new ResponseEntity<>("회원탈퇴여부 Y 변경", HttpStatus.OK);
+//    }
 }
--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -2,19 +2,14 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByUsername(String mail);
-
-    // 이메일로 회원 조회
-    List<Member> findByMail(String email);
-
-    // 회원 탈퇴 (status N -> Y 업데이트)
-    @Modifying
-    @Query("UPDATE Member m SET m.isDelete = 'Y' WHERE m.id = :memberId")
-    void deleteMemberById(@Param("memberId") Long memberId);
+    Optional<Member> findByUsername(String email);
 }
