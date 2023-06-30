42bd309df1a1910bb57e99c778001768223e195c
siyh22 <pooh9609@naver.com>
2023-06-30 21:52:23 +0900

    2023.06.30 Festival backend 진행중

--- src/main/java/com/example/demo/entity/Festival.java
+++ src/main/java/com/example/demo/entity/Festival.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,6 +11,7 @@ import java.util.Date;
 @Entity
 @Getter @Setter @ToString
 @Table(name = "t_festival")
+@Builder
 public class Festival {
     @Id
     @Column(name = "festival_id")
@@ -19,26 +21,47 @@ public class Festival {
     @Column(nullable = false, name = "festival_name")
     private String festivalName;
 
-    @Column(name = "festival_location", nullable = false)
+    @Column(name = "festival_location")
     private String festivalLocation;
 
-    @Column(name = "start_date", nullable = false)
+    @Column(name = "festival_doro")
+    private String festivalDoro;
+
+    @Column(name = "start_date")
     private Date startDate;
 
-    @Column(name = "end_date", nullable = false)
+    @Column(name = "end_date")
     private Date endDate;
 
-    @Column(name = "description", nullable = false)
+    @Column(name = "description")
     private String festivalDesc;
 
-    @Column(name = "main_org", nullable = false)
+    @Column(name = "main_org")
     private String mainOrg;
 
-    @Column(name = "festival_tel", nullable = false)
+    @Column(name = "wedo")
+    private String wedo;
+
+    @Column(name = "kyungdo")
+    private String kyungdo;
+
+    @Column(name = "festival_tel")
     private String festivalTel;
 
-    @Column(name = "festival_img", nullable = false)
+    @Column(name = "festival_img")
     private String festivalImgLink;
 
-
+    public Festival (String festivalName, String festivalLocation, String festivalDoro, Date startDate, Date endDate, String festivalDesc, String mainOrg, String wedo, String kyungdo, String festivalTel, String festivalImgLink) {
+        this.festivalName = festivalName;
+        this.festivalLocation = festivalLocation;
+        this.festivalDoro = festivalDoro;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.festivalDesc = festivalDesc;
+        this.mainOrg = mainOrg;
+        this.wedo = wedo;
+        this.kyungdo = kyungdo;
+        this.festivalTel = festivalTel;
+        this.festivalImgLink = festivalImgLink;
+    }
 }
---
+++ src/main/java/com/example/demo/repository/FestivalRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FestivalRepository {
+
+}
