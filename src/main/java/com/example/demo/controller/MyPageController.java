734174985f483271e440cc5060f3812ce3b9d3cd
qhwkal1 <qhwkal1@naver.com>
2023-07-03 20:46:13 +0900

    0703 마이페이지 API 수정

--- src/main/java/com/example/demo/controller/MyPageController.java
+++ src/main/java/com/example/demo/controller/MyPageController.java
@@ -24,14 +24,14 @@ import java.util.Map;
 public class MyPageController {
     private final MyPageService myPageService;
 
-    // 이메일로 회원 조회
+    // 이메일로 회원 조회 API
     @GetMapping(value = "/email")
     public ResponseEntity<List<MemberDTO>> getMemberInfoByEmail(@RequestParam String email) {
         List<MemberDTO> memberList = myPageService.getMemberByEmail(email);
         return new ResponseEntity<>(memberList, HttpStatus.OK);
     }
 
-    // 회원 닉네임 수정
+    // 회원 닉네임 수정 API
     @PostMapping(value = "/nickname")
     public ResponseEntity<Boolean> updateNickname(@RequestBody Map<String, Object> updateData) {
         String email = (String)updateData.get("username");
@@ -40,14 +40,14 @@ public class MyPageController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    // 닉네임 중복값 체크
+    // 닉네임 중복값 체크 API
     @GetMapping("/nicknameCheck")
     public ResponseEntity<Boolean> checkNicknameAvailability(@RequestParam String nickname) {
         boolean isAvailable = myPageService.isNicknameAvailable(nickname);
         return ResponseEntity.ok(isAvailable);
     }
 
-    // 회원 탈퇴
+    // 회원 탈퇴 API
     @PostMapping(value = "/del")
     public ResponseEntity<Boolean> deleteMember(@RequestBody Map<String, Object> updateData) {
         String email = (String) updateData.get("username");
@@ -55,7 +55,7 @@ public class MyPageController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    // 회원 주소 수정
+    // 회원 주소 수정 API
     @PostMapping(value = "/updateAdd")
     public ResponseEntity<Boolean> updateAddress(@RequestBody Map<String, Object> updateData) {
         String email = (String) updateData.get("username");
@@ -64,42 +64,42 @@ public class MyPageController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    // 동일 회원 주소 중복값 체크
+    // 동일 회원 주소 중복값 체크 API
     @GetMapping("/addressCheck")
     public ResponseEntity<Boolean> checkAddressDuplicate(@RequestParam("email") String email, @RequestParam("address") String address) {
         boolean isDuplicate = myPageService.isAddressAlreadyRegisteredForSelf(email, address);
         return new ResponseEntity<>(isDuplicate, HttpStatus.OK);
     }
 
-    // 마이페이지 내 게시글 조회
+    // 마이페이지 내 게시글 조회 API
     @GetMapping("/communities")
     public ResponseEntity<List<CommunityDTO>> getCommunitiesByMemberId(@RequestParam("memberId") Long memberId) {
         List<CommunityDTO> communities = myPageService.getCommunitiesByMemberId(memberId);
         return new ResponseEntity<>(communities, HttpStatus.OK);
     }
 
-    // 마이페이지에서 특정 회원의 게시글 삭제
+    // 마이페이지에서 특정 회원의 게시글 삭제 API
     @DeleteMapping("/deleteMyCommunities")
     public ResponseEntity<Boolean> deleteCommunitiesByMemberId(@RequestParam("memberId") Long memberId) {
         boolean result = myPageService.deleteCommunityPostsByMemberId(memberId);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    // 마이페이지 내 댓글 조회
+    // 마이페이지 내 댓글 조회 API
     @GetMapping("/comments")
     public ResponseEntity<List<CommentDTO>> getCommentsByMemberId(@RequestParam("memberId") Long memberId) {
         List<CommentDTO> comments = myPageService.getCommentByMemberId(memberId);
         return new ResponseEntity<>(comments, HttpStatus.OK);
     }
 
-    // 마이페이지 내 댓글 삭제
+    // 마이페이지 내 댓글 삭제 API
     @DeleteMapping("/deleteMyComments")
     public ResponseEntity<Boolean> deleteCommentsByMemberId(@RequestParam("memberId") Long memberId) {
         boolean result = myPageService.deleteCommentPostsByMemberId(memberId);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    // 마이페이지 내 결제 조회
+    // 마이페이지 내 결제 조회 API
     @GetMapping("/payments")
     public ResponseEntity<List<PaymentDTO>> getPaymentsByMemberId(@RequestParam("memberId") Long memberId) {
         List<PaymentDTO> payments = myPageService.getPaymentByMemberId(memberId);
