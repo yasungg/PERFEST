c46ae4e7e1c5203ffad6cdd2eabee9200460ebf2
woojin <sun65568@hanmail.net>
2023-07-03 15:51:28 +0900

    07/03 뱃지 랭킹 구현

--- src/main/java/com/example/demo/service/BadgeRankingService.java
+++ src/main/java/com/example/demo/service/BadgeRankingService.java
@@ -18,17 +18,34 @@ import java.util.List;
 public class BadgeRankingService {
     private final MemberRepository memberRepository;
 
-    public List<BadgeRankingDTO> getBadgeRankingList(){
-        List<Member> memberList = memberRepository.findAllByOrderByTotalPriceDesc();
+    public List<BadgeRankingDTO> getBadgeRankingList() {
+        List<Member> memberList = memberRepository.findAllByOrderByBadgesDesc();
         List<BadgeRankingDTO> badgeRankingDTOS = new ArrayList<>();
-        for(Member member : memberList) {
+
+        int rank = 1; // 초기 랭킹 순위 설정
+        int previousBadges = memberList.get(0).getBadges(); // 첫 번째 멤버의 뱃지 수로 초기화
+        int previousRank = 1; // 이전 순위 초기화
+
+        for (int i = 0; i < memberList.size(); i++) {
+            Member member = memberList.get(i);
             BadgeRankingDTO badgeRankingDTO = new BadgeRankingDTO();
             badgeRankingDTO.setMemberId(member.getId());
             badgeRankingDTO.setNickname(member.getNickname());
-            badgeRankingDTO.setBadges(member.getTotalPrice());
+            badgeRankingDTO.setBadges(member.getBadges());
+
+
+            if (member.getBadges() < previousBadges) { // 현재 멤버의 뱃지수가 이전 멤버의 뱃지수 보다 작을 경우만 랭킹 순위 업데이트
+                rank = previousRank + 1; // 이전 순위에서 1을 증가시킴
+            }
+
+            badgeRankingDTO.setRank(rank);
             badgeRankingDTOS.add(badgeRankingDTO);
+
+            previousBadges = member.getBadges(); // 이전 멤버의 뱃지 수 업데이트
+            previousRank = rank; // 이전 순위 업데이트
         }
+
         return badgeRankingDTOS;
     }
-
 }
+
