62ad5accd147b5a0479cf319c8b1723fec9733b1
siyh22 <pooh9609@naver.com>
2023-08-01 17:05:12 +0900

    08/01 신형환 지도 스케일바, 데이터 컨트롤바 제거, 코드 리팩토링

--- src/main/frontend/src/components/NaverMap.js
+++ src/main/frontend/src/components/NaverMap.js
@@ -117,59 +117,6 @@ const NaverMap = () => {
     
   }, [centerLatitude, contextLatitude, contextLongitude, contextFstvlNm]);
 
-
-  // 	const [myLocation, setMyLocation] = useState<
-  // 		{ latitude: number, longitude: number } | 'string'
-  // 	>("");
-
-  // 	// 현재 위치 받아오기
-  // 	useEffect(() => {
-  // 		if(navigator.geolocation) {
-  // 			navigator.geolocation.getCurrentPosition((position) => {
-  // 				setMyLocation({
-  // 					latitude: position.coords.latitude,
-  // 					longitude: position.coords.longitude
-  // 				});
-  // 			});
-  // 		} else {
-  // 			window.alert("현재 위치를 알 수 없습니다.")
-  // 		}
-  // 	}, []);
-
-  // 	useEffect(() => {
-  // 		if(typeof myLocation !== "string") {
-  // 			const currentPosition = [myLocation.latitude, myLocation.longitude];
-
-  // 			const map = new naver.maps.Map("map", {
-  // 				center: new naver.maps.LatLng(currentPosition[0], currentPosition[1]),
-  // 				zoomControl: true
-  // 			});
-  // 		}
-  // 	}, [myLocation]);
-
-  // // 내 위치 마커 표시하기
-  // useEffect(() => {
-  // 	if (typeof myLocation !== "string") {
-  // 		const currentPosition = [myLocation.latitude, myLocation.longitude];
-
-  // 		const map = new naver.maps.Map("map", {
-  // 				center: new naver.maps.LatLng(currentPosition[0], currentPosition[1]),
-  // 				zoomControl: true,
-  // 		});
-  // 			const currentMarker = new naver.maps.Marker({
-  // 				position: new naver.maps.LatLng(currentPosition[0], currentPosition[1]),
-  // 				map,
-  // 				// 원하는 이미지로 마커 커스텀
-  // 				// icon: {
-  // 				//     url: pinImage,
-  // 				//     size: new naver.maps.Size(50, 52),
-  // 				//     origin: new naver.maps.Point(0, 0),
-  // 				//     anchor: new naver.maps.Point(25, 26),
-  // 				//   },
-  // 			});
-  // 		}
-  // 	}, [myLocation]);
-
   return (
     <div
       style={{
