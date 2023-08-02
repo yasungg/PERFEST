302ebdd3647650262f7a0affda5a92bd0b8bd456
DH <lovein6114@nate.com>
2023-08-02 20:58:42 +0900

    23.08.02 페스티벌, 카카오페이 추가 수정

--- src/main/frontend/src/pages/Festival.js
+++ src/main/frontend/src/pages/Festival.js
@@ -1,52 +1,38 @@
-import React, { useEffect, useRef, useState, useContext } from "react";
-import { UserContext } from "../context/UserStore";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Container as BaseContainer } from "../components/StandardStyles";
-import HeaderForFestival from "../components/HeaderForFestival";
+import Header from "../components/Header";
 import NaverMap from "../components/NaverMap";
 import FestivalAPI from "../api/FestivalAPI";
 import SearchSideBar from "../components/SearchSideBar";
-import FestivalSearchCategory from "../components/FestivalSearchCategory";
-import Sidebar from "../components/Sidebar";
-import FestivalDetail from "./FestivalDetail";
+import FestivalSearchCategory from "./FestivalSearchCategory";
 
 const BodyContainer = styled.div`
-  width: 100vw;
-  height: calc(100vh - 58px);
+  width: 100%;
+   height: 100%;
   display: flex;
-  position: relative;
-  overflow: hidden;
-  @media screen and (max-width: 767px) {
-    width: 100vw;
-    height: 100vh;
-  }
 `;
 
 const MapContainer = styled.div`
-  display: flex;
-  width: 100%;
-  position: relative;
-  overflow: hidden;
-  @media screen and (max-width: 767px) {
-    width: 100vw;
-    height: 100vh;
-  }
-`;
+   display: flex;
+   width: 100%;
+   position: relative;
+`
 
 const Festival = () => {
-  return (
-    <BaseContainer>
-      <HeaderForFestival />
-      <BodyContainer>
-        <SearchSideBar />
-        <FestivalDetail />
-        <MapContainer>
-            <NaverMap festivalData={festivalData}/>
-            <FestivalSearchCategory setPropsData={setPropsData} />
-        </MapContainer>
-      </BodyContainer>
-      <Sidebar />
-    </BaseContainer>
+   const [propsData, setPropsData] = useState("");
+   const [festivalData, setFestivalData] = useState("");
+
+   return(
+      <div>
+       <Header/>
+        <BodyContainer>
+          <SearchSideBar/>
+               <MapContainer>
+                  <NaverMap festivalData={festivalData}/>
+                  <FestivalSearchCategory setPropsData={setPropsData} />
+               </MapContainer>
+        </BodyContainer>
+      </div>
   );
 };
 export default Festival;
